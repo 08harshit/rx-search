269dd8ce93336d8f87fb2a722eaaa20202259bdd
08harshit <heyharshithere@gmail.com>
2025-02-28 18:38:06 +0530

    feat(search-bar-component): add search bar component

--- src/app/components/search-bar-component/search-bar-component.component.ts
+++ src/app/components/search-bar-component/search-bar-component.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Subject, debounceTime, takeUntil } from 'rxjs';
 
 @Component({
-  selector: 'app-search-bar-component',
-  imports: [],
+  selector: 'app-search-bar',
   templateUrl: './search-bar-component.component.html',
-  styleUrl: './search-bar-component.component.scss'
-})
-export class SearchBarComponentComponent {
-  constructor()
-  {
+  styleUrls: ['./search-bar-component.component.scss'],
+  imports: [ReactiveFormsModule]
 
+})
+export class SearchBarComponent implements OnInit, OnDestroy {
+  searchControl = new FormControl(''); // Form control for input
+  @Output() searchQuery = new EventEmitter<string>(); // Emit search term
+  private destroy$ = new Subject<void>(); // Cleanup stream
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges
+      .pipe(
+        debounceTime(300), // Wait 300ms before emitting
+        takeUntil(this.destroy$) // Auto-unsubscribe on destroy
+      )
+      .subscribe(query => this.searchQuery.emit(query ?? ''));
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
